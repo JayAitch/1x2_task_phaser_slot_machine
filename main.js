726572ad76066d84c709911695203c4991b99e5c
Jordan <harrisonjordanjames@gmail.com>
2020-07-04 10:17:41 +0100

    displaying all the assets, play animations

--- main.js
+++ main.js
@@ -197,7 +197,10 @@ let config  = {
                     sceneKey: 'spine'
                 }
             ]
-        }
+        },
+        preload:preload,
+        create: create,
+        update:update
     }
 }
 
@@ -206,3 +209,41 @@ let game
 window.addEventListener('load', function() {
     game = new Phaser.Game(config)
 })
+
+function preload(){
+    // localise file location to symbols folder
+    this.load.setPath('./assets/symbols/')
+    // cache spines
+    this.load.spine('cherry', 'symbol_00.json',['symbol_00.atlas'], true);
+    this.load.spine('lemon', 'symbol_01.json',['symbol_01.atlas'], true);
+    this.load.spine('orange', 'symbol_02.json',['symbol_02.atlas'], true);
+    this.load.spine('plumb', 'symbol_03.json',['symbol_03.atlas'], true);
+    this.load.spine('grapes', 'symbol_04.json',['symbol_04.atlas'], true);
+    this.load.spine('melon', 'symbol_05.json',['symbol_05.atlas'], true);
+}
+
+function create() {
+
+     // render all symbols, play animations
+    let cherry = this.add.spine(100, 400, 'cherry');
+    cherry.play("win", true);
+
+    let lemon = this.add.spine(200, 400, 'lemon');
+    lemon.play("win", true);
+
+    let orange = this.add.spine(300, 400, 'orange');
+    orange.play("win", true);
+
+    let plumb = this.add.spine(400, 400, 'plumb');
+    plumb.play("win", true);
+
+    let grapes = this.add.spine(500, 400, 'grapes');
+    grapes.play("win", true);
+
+    let melon = this.add.spine(600, 400, 'melon');
+    melon.play("win", true);
+
+}
+function update(){
+
+}
